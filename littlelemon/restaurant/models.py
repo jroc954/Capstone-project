8972c590739f586434acf9feb97231879d8041fd
Josh R <jroc_offtop@yahoo.com>
2023-12-14 11:17:54 -0500

    model update

--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Bookingtable(models.Model):
+    name = models.CharField(max_length=100)
+    number_of_guests = models.IntegerField(null=False)
+    booking_date = models.DateField()
+    def __str__(self):
+        return self.name
+    
+class Menu(models.Model):
+   title = models.CharField(max_length=200) 
+   price = models.DecimalField(max_digits=6, decimal_places=2)
+   inventory = models.IntegerField(null=False)
+
+   def __str__(self):
+      return self.name   
